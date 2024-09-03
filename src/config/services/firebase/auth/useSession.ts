5c3369cbbda8ab9c51d0816d19d3f9fe6a3648b8
lucasbertolo <lucasbertolo2@gmail.com>
2024-09-03 14:09:01 -0300

    fix: android login flow

--- src/config/services/firebase/auth/useSession.ts
+++ src/config/services/firebase/auth/useSession.ts
@@ -5,7 +5,17 @@ export const useFirebaseSession = () => {
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState<FirebaseAuthTypes.User>();
 
+  const checkAuthStatus = async () => {
+    const currentUser = auth().currentUser;
+
+    if (currentUser) setUser(currentUser);
+
+    if (initializing) setInitializing(false);
+  };
+
   useEffect(() => {
+    checkAuthStatus();
+
     const subscriber = auth().onAuthStateChanged((newUser) => {
       if (newUser) setUser(newUser);
       if (initializing) setInitializing(false);
